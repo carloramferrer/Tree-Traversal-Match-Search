a95939bcedefb53f8c5e894a93ac62918d959f65
carloferrer <carlooferrer@gmail.com>
2019-07-01 19:12:11 +0800

    revised printing

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Tree_Traversal
         static void Main(string[] args)
         {
             // Declare goal state, fringe, and visited list
-            int goalstate = 90;
+            int goalstate = 43;
             List<int> fringe = new List<int>();
             List<int> visited = new List<int>();
 
@@ -17,23 +17,39 @@ namespace Tree_Traversal
             fringe.Add(state);
             while (state <= goalstate)
             {
+                // Show Tree
+                Console.WriteLine("Tree Traversal:");
+                visited.ForEach(item => Console.Write(item + ", "));
+                Console.WriteLine();
+
                 // Add children
                 int lastnode = fringe[fringe.Count-1];
                 fringe.Add(lastnode + 1);
                 fringe.Add(lastnode + 2);
                 fringe.Add(lastnode + 3);
 
-                // Remove fringe and change new state
+                // Change to new state
                 int visitednode = fringe[0];
                 visited.Add(visitednode);
+                
+                // Show Fringe
+                Console.WriteLine("Fringe:");
+                fringe.ForEach(item => Console.Write(item + ", "));
+                Console.WriteLine();
+
+                // Remove fringe and
                 fringe.RemoveAt(0);
                 state = fringe[0];
+                
+                
             }    
-            Console.WriteLine("Fringe:");
-            fringe.ForEach(item => Console.Write(item + ", "));
-            Console.WriteLine();
-            Console.WriteLine("Tree Traversal:");
-            visited.ForEach(item => Console.Write(item + ", "));
+            // Print Final Output:
+            // Console.WriteLine("Fringe:");
+            // fringe.ForEach(item => Console.Write(item + ", "));
+            // Console.WriteLine();
+            // Console.WriteLine("Tree Traversal:");
+            // visited.ForEach(item => Console.Write(item + ", "));
+            // Console.WriteLine();
 
             // Generate Tree
             // int count = 0;
